d9b6daad1b92a56ab36ca145d1caf49cbcf90d4f
Josip Rezić <josip.rezic@hotmail.com>
2018-09-04 19:51:36 +0200

    Initial commit

---
+++ src/com/josiprezic/projecteuler/Main.java
@@ -0,0 +1,8 @@
+package com.josiprezic.projecteuler;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Task1.run();
+    }
+}
---
+++ src/com/josiprezic/projecteuler/Task1.java
@@ -0,0 +1,12 @@
+package com.josiprezic.projecteuler;
+
+public class Task1 {
+    static void run() {
+        printSomething();
+    }
+
+    private static void printSomething() {
+        System.out.println("Co tam Ewicika");
+    }
+
+}
