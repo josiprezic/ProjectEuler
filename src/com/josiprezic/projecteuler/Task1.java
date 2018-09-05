9203d01ad7da00ded7c3110d0f9ce1822fa15457
Josip Rezić <josip.rezic@hotmail.com>
2018-09-05 20:37:43 +0200

    solve task1

--- src/com/josiprezic/projecteuler/Task1.java
+++ src/com/josiprezic/projecteuler/Task1.java
@@ -1,12 +1,19 @@
 package com.josiprezic.projecteuler;
 
+/*
+* Problem 1
+* Multiples of 3 and 5
+*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+* The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+* */
+
 public class Task1 {
+
     static void run() {
-        printSomething();
+        int sum = 0;
+        for(int i = 0; i < 1000; i++) {
+            sum += (i%3 == 0 || i%5 == 0) ? i : 0;
+        }
+        System.out.println("SUM: " + sum);
     }
-
-    private static void printSomething() {
-        System.out.println("Co tam Ewicika!");
-    }
-
 }
